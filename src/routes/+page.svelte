84518349ef8df901bbbcc9274a6a4546309e43ea
Dustin Carlino <dabreegster@gmail.com>
2024-01-19 09:58:01 +0000

    svelte-maplibre!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<script lang="ts">
+    import { MapLibre } from "svelte-maplibre";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<MapLibre
+  style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+  standardControls
+/>
