d585e0e5962fdfa581e9cc91f60b4cad819ce43a
Dustin Carlino <dabreegster@gmail.com>
2024-05-17 12:44:20 +0100

    Add in the two-column layout with SplitComponent pattern

---
+++ ua_components/src/lib/two_column_layout/SplitComponent.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { mapContents, sidebarContents } from './index.js';
+
+	// The kinda weird hack is that this must itself be nested underneath the
+	// MapLibre bit, so it has context. The sidebar is the "remote" part.
+</script>
+
+<div bind:this={$sidebarContents}>
+	<slot name="sidebar" />
+</div>
+<div bind:this={$mapContents}>
+	<slot name="map" />
+</div>
---
+++ ua_components/src/lib/two_column_layout/index.ts
@@ -0,0 +1,7 @@
+import { writable, type Writable } from 'svelte/store';
+
+export { default as Layout } from './Layout.svelte';
+export { default as SplitComponent } from './SplitComponent.svelte';
+
+export let sidebarContents: Writable<HTMLDivElement | null> = writable(null);
+export let mapContents: Writable<HTMLDivElement | null> = writable(null);
