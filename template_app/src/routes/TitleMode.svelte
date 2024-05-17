0896aa5dc8b247cb00f64c28d327ee98a9415447
Dustin Carlino <dabreegster@gmail.com>
2024-05-17 14:57:54 +0100

    Add a slow loading step with progress screen

--- template_app/src/routes/TitleMode.svelte
+++ template_app/src/routes/TitleMode.svelte
@@ -1,13 +1,43 @@
 <script lang="ts">
-	import { SimpleComponent } from 'ua-components';
+	import { SimpleComponent, Loading } from 'ua-components';
 	import { SplitComponent } from 'ua-components/two_column_layout';
-	import { mode } from './globals';
+	import { backend, mode, isLoaded } from './globals';
+	import * as Comlink from 'comlink';
+
+	let loading: string[] = [];
+
+	let fileInput: HTMLInputElement;
+	async function loadFile(e: Event) {
+		try {
+			let file = fileInput.files![0];
+			loading = [`Loading input from ${file.name}`];
+			let buffer = await file.arrayBuffer();
+			await $backend!.loadInput(new Uint8Array(buffer), Comlink.proxy(progressCb));
+			$isLoaded = true;
+			$mode = { kind: 'triangle' };
+		} catch (err) {
+			window.alert(`Couldn't open this file: ${err}`);
+		}
+		loading = [];
+	}
+
+	function progressCb(msg: string) {
+		loading = [...loading, msg];
+	}
 </script>
 
 <SplitComponent>
 	<div slot="sidebar">
 		<SimpleComponent name="Stu" />
-		<button on:click={() => ($mode = { kind: 'triangle' })}>Triangle mode</button>
+
+		<div>
+			<label>
+				Load some large file to start:
+				<input bind:this={fileInput} on:change={loadFile} type="file" />
+			</label>
+		</div>
 	</div>
 	<div slot="map"></div>
 </SplitComponent>
+
+<Loading {loading} />
