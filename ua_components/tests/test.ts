076a747c820ee1e9dab7b1e119967aa1ef841ecc
Dr Griffith Rees <griff.rees@gmail.com>
2024-05-24 12:18:01 +0100

    test: comment out `ua_components/tests/tests.ts` `index page h1` test

--- ua_components/tests/test.ts
+++ ua_components/tests/test.ts
@@ -1,8 +1,9 @@
 import { expect, test } from "@playwright/test";
 
-test("index page has expected h1", async ({ page }) => {
-  await page.goto("/");
-  await expect(
-    page.getByRole("heading", { name: "Welcome to SvelteKit" }),
-  ).toBeVisible();
-});
+// Below fails while `ua_components` is still in library mode
+// test("index page has expected h1", async ({ page }) => {
+//   await page.goto("/");
+//   await expect(
+//     page.getByRole("heading", { name: "Welcome to SvelteKit" }),
+//   ).toBeVisible();
+// });
