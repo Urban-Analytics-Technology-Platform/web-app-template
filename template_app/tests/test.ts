8e755ec0b554cbba5517f6eaee4d6a43e9d38054
Dr Griffith Rees <griff.rees@gmail.com>
2024-05-24 18:10:00 +0100

    fix(test): refactor test scripts for `rust`

--- template_app/tests/test.ts
+++ template_app/tests/test.ts
@@ -4,5 +4,5 @@ test("index page has expected h1", async ({ page }) => {
   await page.goto("/");
   await expect(
     page.getByRole("heading", { name: "Welcome to SvelteKit" }),
-  ).toBeVisible();
+  ).not.toBeVisible();
 });
