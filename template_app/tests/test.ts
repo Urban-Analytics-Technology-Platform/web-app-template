fe027cfbec871da1c00e751a44e49b0d07c40ed1
Dr Griffith Rees <griff.rees@gmail.com>
2024-05-28 17:27:22 +0100

    test: add `Hi Stu` test

--- template_app/tests/test.ts
+++ template_app/tests/test.ts
@@ -1,8 +1,15 @@
 import { expect, test } from "@playwright/test";
 
-test("index page has expected h1", async ({ page }) => {
+test("index page doesn't have default h1", async ({ page }) => {
   await page.goto("/");
   await expect(
     page.getByRole("heading", { name: "Welcome to SvelteKit" }),
   ).not.toBeVisible();
+});
+
+test("index page has 'Hi Stu' h3", async ({ page }) => {
+  await page.goto("/");
+  await expect(
+    page.getByRole("heading", { name: "Hi Stu" }),
+  ).toBeVisible();
 });
